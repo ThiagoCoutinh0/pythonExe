5dbe6ad73220cc80ecdd7da8d1f1d6126fbc2f37
Thiago <thiagoalvesdq@gmail.com>
2023-05-19 17:49:14 -0300

    python

---
+++ Aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um valor:'))
+n2 = int(input('digite outro valor:'))
+s: int = n1 + n2
+#print('A soma de', n1, 'e', n2, 'é', s)
+print(' a soma de {} e {} vale {}'.format(n1,n2,s))
