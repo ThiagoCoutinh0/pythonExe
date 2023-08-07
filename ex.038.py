2aeccbd9c23009461db1796e79f9015fff555641
ThiagoCoutinh0 <thiagoalvesdq@gmail.com>
2023-08-07 20:17:28 -0300

    .

---
+++ ex.038.py
@@ -0,0 +1,8 @@
+p = float(input('Primeiro número:'))
+s = float(input('Segundo número:'))
+if p > s:
+    print('O primeiro número é maior')
+elif s > p:
+    print('O segundo número é maior')
+else
+    print('Os números são iguais')
